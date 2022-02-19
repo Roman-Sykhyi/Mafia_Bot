4c1619b8000da5afc25a401f601e9d580b01c282
Roman-Sykhyi <kariys2015@gmail.com>
2022-02-19 18:15:26 +0200

    test

--- BoryslavMafia_Bot/Configs/BotConfiguration.cs
+++ BoryslavMafia_Bot/Configs/BotConfiguration.cs
@@ -1,6 +1,6 @@
 public static class BotConfiguration
 {
-    public readonly static string Name = "boryslavmafia_bot";
+    public readonly static string Name = "boryslavmafia_debug_bot";
 
-    public readonly static string Key = "5144859609:AAGEXpZhUnNkuk_89NpGh2kPEOm4pGDLQaE";
+    public readonly static string Key = "1746276450:AAFtVwC0-VizoVnqACeZrYe_3FV8m7jiF8Q";
 }
